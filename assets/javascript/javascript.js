502ac9d56fa8f363c6873817d2c6ab2a07b281f8
Chris Valenzuela <chriss.valenzuela@yahoo.com>
2019-04-23 16:31:30 -0700

    fixed syntax and added meta for http request to test

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,13 +4,16 @@ $.ajax({
   method: "GET"
 })
 .then(function(response){
-  var title = response.contents.quotes[0].title;
+
+  //var title = response.contents.quotes[0].title;
   var quote = response.contents.quotes[0].quote;
   var author = response.contents.quotes[0].author;
 
   var fullQuote = ' " ' + quote + ' " ';
   var fullAuthor = " - " + author;
   console.log(response.contents.quotes[0]);
+
   $("#quotes").append(fullQuote);
   $("#quotes").append(fullAuthor);
+
 })
