14ef6bdc7e052a53be51444f8934e17071e70c58
Chris Valenzuela <chriss.valenzuela@yahoo.com>
2019-04-23 15:23:57 -0700

    Most likely the way I want to template it... still need to organize

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,16 @@
 
+$.ajax({
+  url:"http://quotes.rest/qod.json",
+  method: "GET"
+})
+.then(function(response){
+  var title = response.contents.quotes[0].title;
+  var quote = response.contents.quotes[0].quote;
+  var author = response.contents.quotes[0].author;
 
-fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&jsonp=?')
-  .then(res => res.json())
-  .then(post => console.log(post))
+  var fullQuote = ' " ' + quote + ' " ';
+  var fullAuthor = " - " + author;
+  console.log(response.contents.quotes[0]);
+  $("#quotes").append(fullQuote);
+  $("#quotes").append(fullAuthor);
+})
